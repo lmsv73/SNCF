5e1b2f1d5cc7cbbd8a048fbd0578435ffe326d8e
lmsv73 <BPN6AD49a>
2018-02-04 19:41:49 +0100

    Calcul et affichage des distances pour chaque trajets

--- src/app/journey/journey.component.ts
+++ src/app/journey/journey.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {MessageService} from '../message.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {xml2json} from 'xml-js';
 
 @Component({
   selector: 'app-journey',
@@ -15,6 +16,8 @@ export class JourneyComponent  {
   dateDepart = null;
   sections = null;
   timeDiff = null;
+  distance = 0;
+  promises = [];
 
   private _options = {
     headers: new HttpHeaders()
@@ -39,11 +42,33 @@ export class JourneyComponent  {
     let latB = this.oArrivee.stop_area.coord.lat;
     let date = this.dateDepart.toISOString();
 
+    this.distance = 0;
+
     this.http
       .get("https://api.sncf.com/v1/coverage/sncf/journeys?from="+ lonA + ";" + latA + "&to=" + lonB + ";" + latB + "&datetime=" + date, this._options)
       .subscribe(res => {
         this.journeys = res['journeys'];
+        this.calculDistanceTotal();
 
+
+        Promise.all(this.promises).then(val => {
+          console.log(val);
+          let index = 0;
+          let savePoint = 0;
+
+          this.journeys.forEach(elem => {
+            let sections = elem.sections;
+            for(let i = 0; i < sections.length; i++) {
+              if(sections[i].type != "waiting") {
+                index++;
+              }
+            }
+
+            elem["dist"] = Math.round((val[index - 1] - savePoint) / 1000);
+            savePoint = val[index - 1];
+          });
+          this.promises = [];
+        });
       });
   }
 
@@ -77,5 +102,65 @@ export class JourneyComponent  {
     ;
 
     return new Date(y , m, d, h, min, s);
+  }
+
+  getDistance(lonA: number, latA: number, lonB: number, latB: number) {
+    let sr =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:exam="http://example/">
+          <soapenv:Header/>
+          <soapenv:Body>
+             <exam:calculDistance> 
+                <arg0>`+ latA +`</arg0>
+                <arg1>`+ lonA +`</arg1>
+                <arg2>`+ latB +`</arg2>
+                <arg3>`+ lonB +`</arg3>
+             </exam:calculDistance>
+          </soapenv:Body>
+       </soapenv:Envelope>`;
+
+    let promise = new Promise((resolve) => {
+      this.http
+        .post("http://localhost:8080/SOAP_distance_war_exploded/services/CalculDistance?wsdl", sr,
+          {headers: new HttpHeaders().set('Content-Type', 'text/xml'), responseType: 'text'})
+        .subscribe(res => {
+          let json = xml2json(res);
+          let parse = JSON.parse(json).elements[0].elements[0].elements[0].elements[0].elements[0].text;
+          this.distance += parseInt(parse);
+
+          resolve(this.distance);
+        });
+    });
+
+    this.promises.push(promise);
+  }
+
+  calculDistanceTotal() {
+    let lonA,latA, lonB, latB;
+
+    this.journeys.forEach(elem => {
+      elem.sections.forEach(section => {
+        let from = section.from;
+        let to = section.to;
+        if(section.type != "waiting") {
+          if(from.embedded_type == "address") {
+            latA = from.address.coord.lat;
+            lonA = from.address.coord.lon;
+          } else if(from.embedded_type == "stop_point"){
+            latA = from.stop_point.coord.lat;
+            lonA = from.stop_point.coord.lon;
+          }
+
+          if(to.embedded_type == "address") {
+            latB = to.address.coord.lat;
+            lonB = to.address.coord.lon;
+          } else if(to.embedded_type == "stop_point"){
+            latB = to.stop_point.coord.lat;
+            lonB = to.stop_point.coord.lon;
+          }
+
+          this.getDistance(lonA, latA, lonB, latB);
+        }
+      });
+    });
   }
 }
