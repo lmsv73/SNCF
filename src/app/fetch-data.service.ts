472f15b9ae3284f2e1a30dbbd156533c19513b06
lmsv73 <BPN6AD49a>
2018-02-23 09:14:05 +0100

    Suppression de la conversion xml->json lors des appels SOAP

--- src/app/fetch-data.service.ts
+++ src/app/fetch-data.service.ts
@@ -62,7 +62,7 @@ export class FetchDataService {
 
   getCurrencyRate(from: string, to: string, date: string) {
     let sr =
-      `<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+      `<soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
         <soap12:Body>
           <GetConversionRate xmlns="http://tempuri.org/">
             <CurrencyFrom>` + from + `</CurrencyFrom>
